80c1564feb507d2898db00e73fda4badfc1de9df
mwunnam <mwunnam@gmail.com>
2024-01-21 07:34:57 +0000

    done with task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * swap - Function that swaps two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: void
+ **/
+
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - bubble sort function
+ * @array: Array to be sorted
+ * @size: Size of array to be sorted
+ * Return: void
+ **/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+				swap(&array[j], &array[j + 1]);
+
+			print_array(array, size);
+		}
+	}
+}
