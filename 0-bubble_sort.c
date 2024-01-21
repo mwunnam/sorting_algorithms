588707567b4ece95764a54c3d2db99d427cc048a
mwunnam <mwunnam@gmail.com>
2024-01-21 23:14:46 +0000

    correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,6 +28,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1 - i; j++)
